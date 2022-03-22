71240c47a367c72f6d22fb9455e7ea590f1936ed
monman53 <monman.cs@gmail.com>
2022-03-22 23:21:49 +0900

    Initial original page

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    // <div className="App">
+    //   <header className="App-header">
+    <div>
+      <header>
+        <h1>
+          exstitch
+        </h1>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -17,7 +22,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
